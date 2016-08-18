c3a5a428051593c16cc1d7ec2a4318fbfb8a2c1c
Taeyoung Kim <Taeyoung Kim>
2016-08-19 00:17:00 +0900

    test main page

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,21 @@ var loginCheck = require('../util/login-check');
 
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.get('/login_success', loginCheck, function (req, res) {
-  res.render('main', { token: req.user });
+router.get('/login_success', function (req, res) { // loginCheck, function (req, res) {
+  req.session.accessToken = req.user;
+  res.render('main', { token: String('test') }); // req.user });
 });
 
 router.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
+});
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
