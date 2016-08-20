0c7bfdf5be928d8f7447db7940d477541617347e
Taeyoung Kim <Taeyoung Kim>
2016-08-20 22:18:44 +0900

    add service model

---
+++ public/js/model/userModel.js
@@ -0,0 +1,11 @@
+
+function User(_id, name, nickname, gender, likeDrama, joinedChatroom, email, birthday) {
+    this._id = _id;
+    this.name = name;
+    this.nickname = nickname;
+    this.gender = gender;
+    this.likeDrama = likeDrama;
+    this.joinedChatroom = joinedChatroom;
+    this.email = email;
+    this.birthday = birthday;
+}
---
+++ public/js/service/dramaService.js
@@ -0,0 +1,37 @@
+tschedularApp.service('dramaService', function ($http) {
+  this.retrieveDramas = function (timeStart, timeEnd) {
+    return new Promise(function (resolve, reject) {
+
+      var dramalist = [];
+
+      $http({
+        method: "GET",
+        url: HOST_URL + "/api/v1/dramas?airtimeStart="
+        + timeStart + '&airtimeEnd='
+        + timeEnd
+      }).then(function mySuccess(response) {
+        var res = response.data;
+        if (0 === res.status) {
+          var dramas = res.value;
+
+          dramalist = dramas.map(function (drama) {
+            var ret = new Drama();
+
+            ret.id = drama._id;
+            ret.title = drama.title;
+            ret.channel = drama.channel;
+
+            return ret;
+          });
+
+          resolve(dramalist);
+        }
+
+      }, function myError(response) {
+        var errorcode = response.statustext;
+        console.log("error : ".errorcode);
+        reject();
+      });
+    });
+  }
+});
