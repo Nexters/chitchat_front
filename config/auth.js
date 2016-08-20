ec4b68f8b52c5701c8f13ed9b5dc49e897feebb9
Taeyoung Kim <Taeyoung Kim>
2016-08-20 20:32:58 +0900

    config

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@
 module.exports = {
     clientID: '1582785962016025',
     clientSecret: 'c6fb8eb397dfa2628c0c27cd0edd0191',
-    callbackURL: 'http://localhost/auth/facebook/callback',
+    callbackURL: 'http://52.79.81.243/auth/facebook/callback',
     profileFields: ['email', 'name', 'gender']
 };
