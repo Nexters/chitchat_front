f18ab564aee250db5882ae4d3fcd1869a09204dd
TAKDAIM <dim0409@gmail.com>
2016-08-20 22:20:28 +0900

    comomo

--- public/js/chatroom/chat-room.js
+++ public/js/chatroom/chat-room.js
@@ -1,39 +1,29 @@
+<script src="/test.js"></script>
 
-// if (window.sessionStorage) {
-//     var uid = null;
-//     uid = sessionStorage.getItem("chatroom_id");
-//     if (uid == null) {
-//         alert("nono");
-//         // 처리코드
-//         sessionStorage.clear();
-           
-//     } else {
-//         // 정상코드
-//         alert(uid);
-//       //  sessionStorage.clear();
-//     }
-// }
-// else {
-//     // 처리코드
-// }
+tchatroomApp = angular.module('tchatroomApp',[]);
 
- tchatroomApp = angular.module('tchatroomApp',[]);
+var socket = io('52.79.81.243:5003');
 
 tchatroomApp.controller('tchatroomCtrl',['$scope', function($scope, ChatService){
     $scope.chatroomid =location.search.substring(1);
     //$scope.chatContent = "";
     console.log($scope.chatroomid);
+    $scope.mychat = "";
+    $scope.sendChat = function(){
+        //alert($scope.mychat);
+            var result = socket.emit('sendText', $scope.mychat);
+            alert(result);
+            // $('#messages').append($('<li>').text($scope.mychat));
+            // $('#m').val('');
+            return false;
+     //   });
 
+    }
+
+    $scope.getChat = function(){
+
+    }
 }]);
-
-// var tchatroomApp = angular.module('tchatroomApp',[]);
-
-// tchatroomApp.controller('tchatroomCtrl',['$scope','ChatService', function($scope, ChatService){
-//     $scope.chatroomid = "2222";
-//     //location.search.substring(1);
-//     //$scope.chatContent = "";
-//     console.log("test");
-// }]);
 
 
 
