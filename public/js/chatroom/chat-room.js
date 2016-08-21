b70d1da5f1591a2628e4a9f2f9432627bd3f3aae
Taeyoung Kim <Taeyoung Kim>
2016-08-21 13:44:35 +0900

    test

--- public/js/chatroom/chat-room.js
+++ public/js/chatroom/chat-room.js
@@ -1,30 +1,84 @@
-tchatroomApp = angular.module('tchatroomApp',[]);
+tchatroomApp = angular.module('tchatroomApp', []);
 
-tchatroomApp.controller('tchatroomCtrl',['$scope', function($scope, ChatService){
+tchatroomApp.controller('tchatroomCtrl', function ($scope, $window) {
 
     var socket = io(HOST_URL);
 
-    $scope.chatroomid =location.search.substring(1);
-    //$scope.chatContent = "";
-    console.log($scope.chatroomid);
-    $scope.mychat = "";
-    $scope.sendChat = function(){
-        //alert($scope.mychat);
-            var result = socket.emit('sendText', $scope.mychat);
-            console.log(result);
-            // $('#messages').append($('<li>').text($scope.mychat));
-            // $('#m').val('');
-            return false;
-     //   });
-
+    
+    $scope.clearText = function () {
+        $scope.mychat = "";
     }
 
-    $scope.getChat = function(){
+    function init() {
+        // 변수 초기화
+        $scope.clearText();
 
+        $scope.chatroomid = location.search.substring(1);
+        //$scope.chatContent = "";
+        console.log($scope.chatroomid);
+
+
+        // event handler 추가
+        socket.on('left', function (nickname) {
+            $('#messages').append($('<li>').text(nickname + ': has left'));
+        });
+
+        socket.on('history', function (texts) {
+            $('#messages').append($('<li>').text('history'));
+            for (t of texts) {
+
+                console.log(t);
+
+                $('#messages').append($('<li>').text(t.uid.nickname + ': ' + t.message));
+            }
+            $('#messages').append($('<li>').text('-----------------------'));
+        });
+
+        socket.on('participants', function (participants) {
+            $('#messages').append($('<li>').text('list of participants: '));
+            for (p of participants) {
+                $('#messages').append($('<li>').text(p.nickname));
+            }
+            $('#messages').append($('<li>').text('-----------------------'));
+
+        });
+
+        socket.on('error', function (msg) {
+            $('#messages').append($('<li>').text('error occured on socket!: ' + msg));
+        });
+
+        socket.on('newMsg', function (user, msg) {
+            console.log(user);
+            console.log(msg);
+            $('#messages').append($('<li>').text(msg.message));
+        });
+
+        socket.on('joined', function (nickname) {
+            $('#messages').append($('<li>').text(nickname + ': has joined'));
+            socket.emit('getTexts', 0, 100);
+        });
+
+        // join chat room
+
+        socket.emit('joinRoom', $window.localStorage.token, $scope.chatroomid);
     }
-}]);
 
 
+    $scope.sendChat = function () {
+        console.log($scope.mychat);
 
+        socket.emit('sendText', $scope.mychat);
 
+        $scope.clearText();
 
+        return false;
+    }
+
+    $scope.getChat = function () {
+        socket.emit('getTexts', 0, 100);
+    }
+
+    
+
+    init();
+});
--- public/js/common/url.js
+++ public/js/common/url.js
@@ -1,1 +1,2 @@
 var HOST_URL = "http://52.79.81.243";
+// var HOST_URL = "http://localhost:5003";
